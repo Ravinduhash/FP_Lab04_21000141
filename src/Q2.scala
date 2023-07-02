894579f4b35774f28255f75b87ced0fe0904ce1a
Ravindu Hasanka <rhatu2000@gmail.com>
2023-07-02 22:14:56 +0530

    Question 02 started

---
+++ src/Q2.scala
@@ -0,0 +1,9 @@
+import scala.io.StdIn._
+object Q2 extends App{
+  var value = readLine("Please enter an integer: ").toInt
+  def checkNegetive(value:Int):Boolean={
+    value match{
+      case v if v
+    }
+  }
+}
