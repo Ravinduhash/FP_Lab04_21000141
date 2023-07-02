b0a8efb7a7dc5c694b631ac24f40f05fffb60516
Ravindu Hasanka <rhatu2000@gmail.com>
2023-07-02 21:58:59 +0530

    Question 01

--- src/Q1.scala
+++ src/Q1.scala
@@ -1,3 +1,16 @@
-object Q1 extends App{
+import jdk.jfr.DataAmount
 
+import scala.io.StdIn._
+object Q1 extends App{
+  var value = readLine("Enter the deposit value:").toFloat
+
+  def interest(deposit:Double):Double={
+    deposit match {
+      case d if d<20000 => deposit*2/100
+      case d if d<200000 => deposit*4/100
+      case d if d<2000000 => deposit*3.5/100
+      case _ => deposit*6.5/100
+    }
+  }
+  print("The interest per year is Rs."+interest(value))
 }
