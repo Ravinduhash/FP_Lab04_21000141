903516aaefc124a2b5be2af1a25f0bef5ba262d3
Ravindu Hasanka <rhatu2000@gmail.com>
2023-07-02 22:29:45 +0530

    Question 02

--- src/Q2.scala
+++ src/Q2.scala
@@ -3,7 +3,37 @@ object Q2 extends App{
   var value = readLine("Please enter an integer: ").toInt
   def checkNegetive(value:Int):Boolean={
     value match{
-      case v if v
+      case v if v<=0 => true
+      case _ => false
     }
   }
+  def checkEven(value: Int): Boolean = {
+    value match {
+      case v if v%2 <= 0 => true
+      case _ => false
+    }
+  }
+  def printEven(value:Int):Any={
+    if (checkEven(value)) {
+      println("This is Even Number")
+    }
+    else {
+      println("This is Odd Number")
+    }
+  }
+
+  def printNegetive(value:Int):Any={
+    if (checkNegetive(value)) {
+      println("This is negetive number or 0")
+    }
+    else {
+      println("This is Positive Number")
+      printEven(value)
+    }
+  }
+
+  printNegetive(value)
+
+
+
 }
